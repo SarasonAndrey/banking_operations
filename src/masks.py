fd236aa89935172150bc86cd04b7ec199497592a
Andrey Sarason <sarason@rambler.ru>
2025-03-29 12:33:19 +0300

    add corr mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
-    if len(card_number) != 16 or not card_number.isdigit():
-        raise ValueError("Номер карты должен состоять из 16 цифр.")
+    # if len(card_number) != 16 or not card_number.isdigit():
+    #     raise ValueError("Номер карты должен состоять из 16 цифр.")
     return (
         card_number[0:4]
         + " "
@@ -14,6 +14,6 @@ def get_mask_card_number(card_number: str) -> str:
 
 
 def get_mask_account(mask_account: str) -> str:
-    if len(mask_account) != 20 or not mask_account.isdigit():
-        raise ValueError("Номер счета должен состоять из 20 цифр.")
+    # if len(mask_account) != 20 or not mask_account.isdigit():
+    #     raise ValueError("Номер счета должен состоять из 20 цифр.")
     return "**" + mask_account[-4:]
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,25 @@
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(name_number: str) -> str:
-    pass
+    name_card = ""
+    number_card = ""
+    for i in name_number.split():
+
+        if i.isalpha():
+            name_card += i
+        else:
+            number_card += i
+            if "Счет" in name_card:
+                return f"{name_card} {get_mask_account(name_number)}"
+    return f"{name_card} {get_mask_card_number(number_card)}"
 
 
-def get_date(data):
-    pass
+def get_date(date_string):
+    try:
+        date_obj = datetime.fromisoformat(date_string)
+        return date_obj.strftime("%d.%m.%Y")
+    except ValueError as e:
+        raise ValueError(f"Неверный формат даты: {e}")
