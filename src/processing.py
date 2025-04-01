48c009c287c742020936f834492e7f793d72c242
Andrey Sarason <sarason@rambler.ru>
2025-04-01 20:09:29 +0300

    add readme.md

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,17 @@
-def filter_by_state(list_dictionaries: list, state='EXECUTED') -> list:
+from operator import itemgetter
+
+
+def filter_by_state(list_dictionaries: list, state="EXECUTED") -> list:
     new_list_dictionaries = []
     for i in list_dictionaries:
-        if i['state'] == state:
+        if i["state"] == state:
             new_list_dictionaries.append(i)
     return new_list_dictionaries
 
 
 def sort_by_date(list_dictionaries: list) -> list:
-    pass
+    for i in list_dictionaries:
+        new_list_sort_by_date = sorted(
+            list_dictionaries, key=itemgetter("date"), reverse=True
+        )
+    return new_list_sort_by_date
