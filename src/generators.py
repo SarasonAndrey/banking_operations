5f3d26e4be0d47cdf2608cf9ee25356417b21623
Andrey Sarason <sarason@rambler.ru>
2025-04-12 17:09:34 +0300

    improvement card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -2,20 +2,20 @@ from typing import Any, Generator
 
 
 def filter_by_currency(
-        list_of_transactions: list[dict[str, Any]], code="USD"
+    list_of_transactions: list[dict[str, Any]], code="USD"
 ) -> Generator[dict[str, Any], None, None]:
     for list_of_currencies in list_of_transactions:
         if (
-                list_of_currencies.get("operationAmount", {})
-                        .get("currency", {})
-                        .get("code")
-                == code
+            list_of_currencies.get("operationAmount", {})
+            .get("currency", {})
+            .get("code")
+            == code
         ):
             yield list_of_currencies
 
 
 def transaction_descriptions(
-        list_dictionaries_transactions: list[dict[str, Any]]
+    list_dictionaries_transactions: list[dict[str, Any]]
 ) -> Generator[str, None, None]:
     for description_operation in list_dictionaries_transactions:
         key = description_operation.get("description")
